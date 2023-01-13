dcd55c04d7a2fb714e8ebb76b202405b45d713a4
daniela marques <dd978452@gmail.com>
2023-01-13 14:07:09 -0300

    Adicionar arquivos de projeto.

---
+++ Split/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace exemplo // Definir o pacote do sistema  
+{
+    class Program // Nome do projeto ou pasta  
+    {
+        static void Main(string[] args)  // Definindo a função principal 
+        {
+            string frase = Console.ReadLine(); // recebe uma entrada do usuário  
+            String x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("Você digitou: "); // imprime a saída  
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+        }
+    }
+
+}
